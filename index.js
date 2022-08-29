dc1c5f16664221a0698469a8a4b90716bac322b6
Ashton MacKenzie <pikeminnow@gmail.com>
2022-08-29 17:46:24 -0400

    commit tree d65e3865a45c71f50d9bdb991db7ad9c87644cf3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(suggestion) {
+    if (!suggestion) {
+        suggestion = 'roller-skate';
+    }
+    return `This Saturday, I want to ${suggestion}!`
+}
+function mondayWork(suggestion) {
+    if (!suggestion) {
+        suggestion = 'go to the office';
+    }
+    return `This Monday, I will ${suggestion}.`
+}
+
+function wrapAdjective(highlight="*") {
+    
+    return function innerWrapAdjective(word="special") { 
+        return `You are ${highlight}${word}${highlight}!`;
+    };
+}
